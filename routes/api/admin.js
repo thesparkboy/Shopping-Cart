75bd55cd8824dc06a3d3442129df260717f89972
thesparkboy <madan.aakash2@gmail.com>
2018-06-22 23:31:13 +0530

    UI

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -5,16 +5,4 @@ router.get('/', (req, res) => {
         res.render('admin');
 })
 
-// router.post('/', (req, res) => {
-//     Product.create({
-//         name: req.body.name
-//     }).then((user) => {
-//         res.status(201).send(user)
-//     }).catch((err) => {
-//         res.status(501).send({
-//             error: "Couldn't add new user"
-//         })
-//     })
-// })
-
 exports = module.exports =  router
