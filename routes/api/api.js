0ab661d66493650fa08cbdc68b1bc13aaf122f90
thesparkboy <madan.aakash2@gmail.com>
2018-06-22 22:14:52 +0530

    UI

---
+++ routes/api/api.js
@@ -0,0 +1,65 @@
+const router = require('express').Router()
+const Cart = require('../../db').Cart,
+      Product = require('../../db').Product
+
+router.post('/cartadd', (req, res) => {
+  req.body.id = parseInt(req.body.id);
+  Product.find({ where: { id: req.body.id } }).then(item => {
+    req.body.name = item.name
+    req.body.description = item.description
+    req.body.price = item.price
+    req.body.url = item.url
+  })
+  Cart.find({ where: { id: req.body.id } }).then(item => {
+    if(item){
+        item.quantity = item.quantity + 1;
+        item.save().then(() => {})
+
+    } else {
+      Cart.create({
+          id: req.body.id,
+          name: req.body.name,
+          description: req.body.description,
+          price: parseFloat(req.body.price),
+          quantity: 1,
+          url:req.body.url
+      }).then((product) => {
+
+      }).catch((error) => {
+          res.send({error})
+      })
+    }
+  });
+})
+
+router.post('/incr', (req, res) => {
+  req.body.id = parseInt(req.body.id);
+  Cart.find({ where: { id: req.body.id } }).then(item => {
+      item.quantity = item.quantity + 1;
+      item.save().then(() => {
+        res.send('updated successfully');
+      })
+  });
+})
+
+router.post('/decr', (req, res) => {
+  req.body.id = parseInt(req.body.id);
+  Cart.find({ where: { id: req.body.id } }).then(item => {
+      if(item.quantity == 1){
+        Cart.destroy({ where: { id: req.body.id }})
+        res.send('updated successfully');
+      } else {
+        item.quantity = item.quantity - 1;
+        item.save().then(() => {
+          res.send('updated successfully');
+        })
+      }
+  });
+})
+
+router.get('/placed',(req, res) => {
+  Cart.destroy({ where: {}, truncate: true });
+  res.render('placed');
+})
+
+exports = module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
+router.use('/', require('./api'))
 router.use('/products', require('./product'));
 router.use('/admin', require('./admin'));
 router.use('/cart', require('./cart'));
